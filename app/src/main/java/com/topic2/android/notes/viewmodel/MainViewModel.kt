3b1bae27d797201a5f1ebc7f4b2fd443bc6c39cc
NeroiN <NeroiN2@yandex.ru>
2023-05-28 22:42:03 +0300

    Add note creation function

--- app/src/main/java/com/topic2/android/notes/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/topic2/android/notes/viewmodel/MainViewModel.kt
@@ -2,8 +2,11 @@ package com.topic2.android.notes.viewmodel
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.topic2.android.notes.data.repository.Repository
 import com.topic2.android.notes.domain.model.NoteModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 /**
  * Модель просмотра, используемая для хранения глобального состояния приложения.
@@ -14,4 +17,22 @@ class MainViewModel(private val repository: Repository) : ViewModel() {
     val notesNotInTraesh: LiveData<List<NoteModel>> by lazy {
         repository.getAllNotesNotInTrash()
     }
+
+    fun  onCreateNewNoteClick(){
+        // todo
+    }
+
+    fun onNoteClick(note: NoteModel) {
+        // todo
+    }
+
+    fun onNoteCheckedChange(note: NoteModel) {
+        viewModelScope
+            .launch(
+              Dispatchers.Default
+            ) {
+                repository.insertNote(note)
+            }
+    }
+
 }
